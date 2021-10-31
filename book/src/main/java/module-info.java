133256ce7fce6c7b502ab3c3b523ac290a2241ec
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:55:32 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- book/src/main/java/module-info.java
+++ book/src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-tempfiles-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ao-tempfiles-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoapps.tempfiles.servlet.book {
 	// Direct
