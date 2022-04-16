8f890e7b3ffd7f2b606b7d9697d49f0cc4c29713
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:27 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/tempfiles/servlet/TempFileContextEE.java
+++ src/main/java/com/aoapps/tempfiles/servlet/TempFileContextEE.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-tempfiles-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.tempfiles.servlet;
 
 import com.aoapps.servlet.attribute.AttributeEE;
